2ad68c13a0f1db3ba2500ff87333332aab55e428
sellbytel-dhauptman <sellbytel.dhauptman@premium-cloud-support.com>
2019-02-10 16:35:33 +0100

    deploy to prod

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Changing code to test git tagging')
+        self.response.write('Deployed to prod')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
